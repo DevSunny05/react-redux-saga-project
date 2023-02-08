856754cac7f4a5f0af8c37e59e82a1577289e114
sarang <sarang.office01@gmail.com>
2023-02-08 09:26:31 +0530

    cart page table work done

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 const Cart = () => {
     const cartData=useSelector(state=>state.cartData)
   return (
-    <div>
-        <Link to='/'>Go to Product list</Link>
-        <h1>Cart Page</h1>
-        <div classname="cartItem-container" style={{display:'flex',marginLeft:'30px',marginRight:'30px'}}>
+    <div className='p-4'>
+        <Link to='/' className='' style={{padding:'5px 10px', marginTop:'20px',border:'1px solid lightgray'}}>Go to Product list</Link>
+        
+        <div className="cartItem-container" style={{display:'flex',marginLeft:'30px',marginRight:'30px',flexDirection:'column',marginTop:'40px'}}>
             <table style={{flex:'65%',borderCollapse:'collapse'}}>
                 <tr >
                     <td>Name</td>
@@ -29,11 +29,11 @@ const Cart = () => {
                         )
                 }
             </table>
-            <div className='price-details'>
-                <div className='adjust-price'><span>Amount:</span><span>000</span></div>
-                <div className='adjust-price'><span>Discount:</span><span>000</span></div>
-                <div className='adjust-price'><span>Tax:</span><span>000</span></div>
-                <div className='adjust-price'><span>Total:</span><span>000</span></div>
+            <div className='price-details mt-2 border-collapse border w-full p-2 ' >
+                <div className='adjust-price'><span className='font-bold'>Amount:</span><span>000</span></div>
+                <div className='adjust-price'><span className='font-bold'>Discount:</span><span>000</span></div>
+                <div className='adjust-price'><span className='font-bold'>Tax:</span><span>000</span></div>
+                <div className='adjust-price'><span className='font-bold'>Total:</span><span>000</span></div>
             </div>
         </div>
     </div>
